7558a30535e438e4ccd2a05a731dcee490187ffd
yuki <mail@yuki.sh>
2023-02-22 16:30:27 +0800

    feat: add auto send

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-import { Plugin, Option } from 'kokkoro';
-import { KFCService } from './service';
+import { Plugin, Option } from '@kokkoro/core';
+import { Service } from './service';
 
 interface KFCOption extends Option {
-  // auto_send: boolean;
+  auto_send: boolean;
 }
 
 const option: KFCOption = {
   apply: true,
   lock: false,
+  auto_send: true,
 };
-const { version } = require('../package.json');
+const pkg = require('../package.json');
 const plugin = new Plugin('kfc', option);
-const service = new KFCService();
+const service = new Service(plugin);
 
 plugin
-  .version(version)
+  .version(pkg.version)
+  .schedule(process.env.KFC_DATE ?? '0 0 12 ? * 5', async () => {
+    await service.autoSend();
+  })
 
 plugin
   .command('say')
--- src/service.ts
+++ src/service.ts
@@ -1,10 +1,14 @@
 import axios from 'axios';
+import { Plugin } from '@kokkoro/core';
 
-export class KFCService {
+export class Service {
   /** API */
   api: string;
 
-  constructor() {
+  constructor(
+    /** 插件 */
+    private plugin: Plugin,
+  ) {
     this.api = 'https://kfc.yuki.sh/api/index';
   }
 
@@ -18,5 +22,27 @@ export class KFCService {
       message = (<Error>error).message;
     }
     return message;
+  }
+
+  async autoSend() {
+    const message = await this.getMessage();
+
+    this.plugin.bl.forEach((bot) => {
+      const sendQueue = [];
+      const uins = [...bot.gl.keys()];
+      const uins_length = uins.length;
+
+      for (let i = 0; i < uins_length; i++) {
+        const uin = uins[i];
+        const info = bot.gl.get(uin)!;
+        const { group_id } = info;
+        const { apply, auto_send } = bot.profile.getOption(group_id, 'message');
+
+        if (apply && auto_send) {
+          sendQueue.push(bot.sendGroupMsg(group_id, message));
+        }
+      }
+      Promise.allSettled(sendQueue);
+    });
   }
 }
